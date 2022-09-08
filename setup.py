5775dbf68aa25fbf6d525ff11cc607292641e32e
Piotrek Rybiec <piotrrybiec28@gmail.com>
2022-09-08 17:43:35 +0200

    change package name

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
 with open(os.path.join(directory, 'requirements.txt'), encoding='utf-8') as f:
   requirements = [line for line in f.read().strip().split('\n')]
 
-setup(name='wafflephilib',
+setup(name='wafflephi',
       version='0.1.0',
       description='wafflephi tools library',
       author='Piotr Rybiec',
       license='MIT',
       long_description=long_desc,
       long_description_content_type='text/markdown',
-      packages=['wafflephilib'],
+      packages=['wafflephi'],
       classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License"
