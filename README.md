9a5f7e0324fb51860941b21076cef7f6125ab023
Piotrek Rybiec <piotrrybiec28@gmail.com>
2022-09-21 22:58:25 +0200

    install readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # wafflephi library
+
+Install with:
+
+```
+git clone https://github.com/wafflephi/lib
+cd lib/
+pip3 install -e .
+```
+
--- setup.py
+++ setup.py
@@ -7,10 +7,6 @@ directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
   long_desc = f.read()
 
-
-with open(os.path.join(directory, 'requirements.txt'), encoding='utf-8') as f:
-  requirements = [line for line in f.read().strip().split('\n')]
-
 setup(name='wafflephi',
       version='0.1.0',
       description='wafflephi tools library',
@@ -23,8 +19,13 @@ setup(name='wafflephi',
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License"
       ],
-      install_requires=requirements,
-      python_requires='>=3.9',
+      install_requires=['numpy', 'requests', 'scipy'],
+      python_requires='>=3.10',
+      extras_require={
+        'testing': [
+          "pytest",
+        ],
+      },
       include_package_data=True)
 
 
