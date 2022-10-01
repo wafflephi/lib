6130d1af6193ac350a176d6424eef4f014ee410f
Piotrek Rybiec <piotrrybiec28@gmail.com>
2022-10-01 15:57:59 +0200

    plot: update for new linreg struct

--- wafflephi/plot.py
+++ wafflephi/plot.py
@@ -1,17 +1,20 @@
+#!/usr/bin/env python3
+
 import pandas as pd
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from typing import Optional, List, Union, Iterable
 
-from wafflephi.statistics import LinearRegression
+from wafflephi.statistics import linreg, fit
 
 def trend(df: pd.DataFrame, label:Optional[str]=None, stdev:Optional[bool]=True) -> matplotlib.figure.Figure:
   Y = np.log10(df['Close'])
   X = range(df.shape[0])
-  est, slope, y_int = LinearRegression.lstsq(X,Y)
+  l = linreg(X, Y)
+  est = fit(l, Y)
 
-  fig, ax = plt.subplots(figsize=(30,20))
+  fig, ax = plt.subplots(figsize=(20,10))
 
   DATES_INTERVAL = df.shape[0]//10
   plt.plot(X,Y)
@@ -23,7 +26,6 @@ def trend(df: pd.DataFrame, label:Optional[str]=None, stdev:Optional[bool]=True)
     plt.plot(X, est - np.std(est)/4, "g", label="- 1 STDEV")
     plt.plot(X, est - np.std(est)/2, "g", label="- 2 STDEV")
 
-  plt.yscale('linear')
   legend = plt.legend(loc='upper left', fontsize=18)
   ax.yaxis.set_major_formatter(matplotlib.ticker.FuncFormatter(lambda y, _: '{:g}'.format(10**y)))
 
@@ -36,6 +38,9 @@ def trend(df: pd.DataFrame, label:Optional[str]=None, stdev:Optional[bool]=True)
   return fig
 
 if __name__ == "__main__":
-  df = pd.read_csv('https://stooq.com/q/d/l/?s=btc.v&i=m')
-  fig = trend(df, 'btc.v')
+  import sys
+  ticker = sys.argv[-1]
+  df = pd.read_csv(f'https://stooq.com/q/d/l/?s={ticker}&i=m')
+  print(df)
+  fig = trend(df, ticker)
   plt.show()
