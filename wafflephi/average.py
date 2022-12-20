fd5961bf20aa7329ab94b3e46825cce2bcf2ecdc
Piotrek Rybiec <piotrrybiec28@gmail.com>
2022-12-20 10:10:25 +0100

    averages: change ema to yield not return

--- wafflephi/average.py
+++ wafflephi/average.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
 
+from typing import Iterator
+
 def mean(numbers: list[float]) -> float:
     return float(sum(numbers) / len(numbers))
 
-def ema(data: list[float], period: int, smoothing: int = 2) -> float:
+def ema(data: list[float], period: int, smoothing: int = 2) -> Iterator[float]:
     ret = [sum(data[:period]) / period]
     for x in data[period:]:
-        ret.append(
-            (x * (smoothing / (1 + period)))
-            + ret[-1] * (1 - (smoothing / (1 + period)))
-        )
-    return ret
+        yield (x * (smoothing / (1 + period))) + ret[-1] * (1 - (smoothing / (1 + period)))
 
 if __name__ == "__main__":
     import numpy as np
 
     x = np.arange(-150, 150)
-    ema_ = ema(x, 10)
+    ema_ = list(ema(x, 10))
     print(round(mean(ema_), 2))
